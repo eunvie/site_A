52ebaa10e2acbd64986628763ed999694005f424
eunvie.noh@gmail.com <eunvie.noh@gmail.com>
2023-08-27 14:51:58 +0900

    두번째 커밋

--- js/jquery.js
+++ js/jquery.js
@@ -1,18 +1,49 @@
-const depth1 =$('.depth1');
+const depth1 = $('.depth1');
 depth1.hover(
-    function(){
+    function () {
         $('.depth2').css('display', 'block');
         $('header').addClass('on');
     },
-    function(){
+    function () {
         $('.depth2').css('display', 'none');
         $('header').removeClass('on');
     }
 );
 
+// ----slide----
+// 할당하다 할당연산자
+// 좌항에 우항의 값을 할당
+const slides = $('.slide_wrap li');
+let n = 0;
+function slide() { slides.removeClass('on'); slides.eq(n).addClass('on'); n++; if (n >= slides.length) { n = 0 } }
+setInterval(() => { slide(); }, 2000);
 
-// depth1.addEventListener('mouseover',function(){depth1.querySelectorAll('.depth2').forEach((o)=>{o.style.display='block'})
-// document.querySelector('header').classList.add('on');});
+slides.eq(n).addClass('on');
+
+// ----tab----
+const tabBtn=$('.board .buttons li');
+const panels=$('.panels>div');
+tabBtn.on('click',function(e){
+    e.preventDefault(); let tg = $(this).index();
     
-// depth1.addEventListener('mouseout',function(){depth1.querySelectorAll('.depth2').forEach((o)=>{o.style.display='none'})
-// document.querySelector('header').classList.remove('on');});
+    tabBtn.removeClass('on');
+    tabBtn.eq(tg).addClass('on');
+    
+    panels.hide();
+    panels.eq(tg).show();
+    
+})
+
+// ----popup----
+$('.open').on('click',function(e){
+e.preventDefault();
+/* $('.popup').css('display','block'); */
+$('.popup').show();
+$('.overlay').addClass('open');
+});
+
+$('.close').on('click',function(e){
+e.preventDefault();
+$('.popup').hied();
+$('.overlay').removeClassClass('open');
+});
